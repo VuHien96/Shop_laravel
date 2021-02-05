26220eaa60a38374a2cb9f4195e1f0fe7fcdb993
VuHien96 <vuhien96.jr@gmail.com>
2021-02-05 14:54:44 +0700

    init project

---
+++ resources/views/fragment/aside-admin.blade.php
@@ -0,0 +1,26 @@
+<aside>
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->
+        <div class="leftside-navigation">
+            <ul class="sidebar-menu" id="nav-accordion">
+                <li>
+                    <a class="active" href="index.html">
+                        <i class="fa fa-dashboard"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>Danh mục</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="typography.html">Danh sách danh mục</a></li>
+                        <li><a href="glyphicon.html">Tạo mới</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
